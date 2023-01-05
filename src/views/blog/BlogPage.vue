2971fa179bc704b6c0df39f5ec508f773d26783d
WLLTB <1967475093@qq.com>
2023-01-06 04:07:07 +0800

    【项目代码】预留排行榜位置，调整博客首页样式

--- src/views/blog/BlogPage.vue
+++ src/views/blog/BlogPage.vue
@@ -1,21 +1,40 @@
 <template>
   <div>
-    <el-card v-for='blog in state.blogList' class='box-card'>
-      <template #header>
-        <div class='card-header'>
-          <div class='user-info'>
-            <el-avatar size='small' src='https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'/>
-            &nbsp;
-            <div>{{blog.author}}</div>
-            <div class='update-time'>
-              <el-icon><Clock /></el-icon>
-              {{blog.updateTime}}
-            </div>
-          </div>
+    <el-row>
+      <el-col :span='16'>
+        <div class='blog-list flex'>
+          <el-input placeholder='搜索文章'/>&nbsp;
+          <el-button type='primary'>搜索</el-button>
         </div>
-      </template>
-      <div>{{ blog.content }}</div>
-    </el-card>
+        <el-card v-for='blog in state.blogList' class='blog-list'>
+          <template #header>
+            <div class='card-header'>
+              <div class='user-info'>
+                <el-avatar size='small' src='https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'/>
+                &nbsp;
+                <div>{{ blog.author }}</div>
+                <div class='update-time'>
+                  <el-icon>
+                    <Clock/>
+                  </el-icon>
+                  {{ blog.updateTime }}
+                </div>
+              </div>
+            </div>
+          </template>
+          <div class='title-info'>
+            <h3>{{ blog.title }}</h3>
+            <el-tag size='small'>测试</el-tag>
+          </div>
+          <div>{{ blog.content }}</div>
+        </el-card>
+      </el-col>
+      <el-col :span='8'>
+        <el-card class='rank-list'>
+          11
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -50,9 +69,9 @@ onMounted(() => {
   font-size: 12px;
 }
 
-.box-card {
+.blog-list {
   margin: 25px auto;
-  width: 480px;
+  width: 90%;
 }
 
 .update-time {
@@ -62,5 +81,16 @@ onMounted(() => {
 
   color: #bbb;
   line-height: 100%;
+}
+
+.title-info {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.rank-list {
+  margin: 25px auto;
+  width: 80%;
 }
 </style>
