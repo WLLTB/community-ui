661354620978e8f9e33a4df0bc253ebf65d13931
WLLTB <1967475093@qq.com>
2023-01-06 04:19:50 +0800

    【项目初始化】配置主题

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import MenuBar from './components/MenuBar.vue';
+
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  document.body.style.setProperty('--el-color-primary', '#3447C0');
+  document.body.style.setProperty('--el-color-primary-light-9', '#F5FBF0');
+  document.body.style.setProperty('--el-color-primary-light-3', '#3447C0');
+});
 </script>
 
 <template>
