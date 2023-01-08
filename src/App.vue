7b8ddc1d24c0043302ab88a7b5b82704e08299ba
WLLTB <1967475093@qq.com>
2023-01-08 19:18:41 +0800

    【项目代码】首页添加logo文字，抽取样式文件

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import {RouterView} from 'vue-router';
 import MenuBar from './components/MenuBar.vue';
 import img from './assets/bg.jpg';
 import {onMounted} from 'vue';
+import './assets/app.css';
+const TITLE = 'WLLTB';
 
 onMounted(() => {
   document.body.style.setProperty('--el-color-primary', '#3447C0');
@@ -19,6 +21,11 @@ onMounted(() => {
         <img src='https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png' class='avator'/>
       </a>
     </div>
+    <div class='header-info'>
+      <el-icon v-for='str in TITLE' size='120'>
+        <div class='header-title'>{{str}}</div>
+      </el-icon>
+    </div>
     <div class='header-text'>
       <h2 class='site-master-description'>有趣的灵魂终将相遇</h2>
     </div>
@@ -28,15 +35,5 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.menu {
-  width: 100%;
-  position: absolute;
-  top: 0;
-  color: #FFFFFF;
-}
 
-.header-img {
-  width: 100%;
-  height: 630px;
-}
 </style>
--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,47 +1,9 @@
 <template>
-<!--  <el-menu-->
-<!--      :default-active='activeIndex'-->
-<!--      class='el-menu-demo'-->
-<!--      mode='horizontal'-->
-<!--      text-color='#fff'-->
-<!--      active-text-color='#ffd04b'-->
-<!--      @select='handleSelect'-->
-<!--  >-->
-<!--    <div class='flex'>-->
-<!--      <el-menu-item index='index'>-->
-<!--        首页-->
-<!--      </el-menu-item>-->
-<!--      <el-menu-item index='video'>-->
-<!--        短视频-->
-<!--      </el-menu-item>-->
-<!--      <el-sub-menu index='info'>-->
-<!--        <template #title>个人信息</template>-->
-<!--        <el-menu-item index='logout'>退出登录</el-menu-item>-->
-<!--      </el-sub-menu>-->
-<!--      <div class='flex-item-right'>-->
-<!--        <el-space>-->
-<!--          <el-input placeholder='搜索文章' clearable/>&nbsp;-->
-<!--          <el-button type='primary' :icon='Search'>搜索</el-button>-->
-<!--        </el-space>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </el-menu>-->
-  <div>
+  <div class='flex'>
+
   </div>
 </template>
 
 <script setup>
-import {Search} from '@element-plus/icons-vue';
-
-const activeIndex = 'index';
-const handleSelect = (key, keyPath) => {
-  console.log(key, keyPath);
-};
-
+import '../assets/menu.css';
 </script>
-
-<style scoped>
-.el-menu-demo {
-
-}
-</style>
